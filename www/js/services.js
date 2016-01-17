0f7747f6ec38e46d9db9cc6fb892f5741b202fe2
donte <digitalartcrew@gmail.com>
2016-01-16 21:30:52 -0800

    Test

--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-app.factory('supebowlService', function($http, $q) {
+app.factory('superbowlService', function($http, $q) {
 	return {
 		restaurants: function(){
 			return $http.get('http://localhost:3001/restaurants');
